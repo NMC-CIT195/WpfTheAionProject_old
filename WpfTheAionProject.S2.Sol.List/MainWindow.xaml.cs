6aa9606164a1cda4e951106580210bdc08b9d7cf
John Velis <john@johnvelis.com>
2019-03-17 18:46:40 -0400

    S2.List

--- WpfTheAionProject.S2.Sol.List/MainWindow.xaml.cs
+++ WpfTheAionProject.S2.Sol.List/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfTheAionProject.S2.Sol
+namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
